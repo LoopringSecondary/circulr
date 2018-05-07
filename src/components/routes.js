07f96bdd28e45349808928750b81b88cf528205c
houzongbao <houzongbao@126.com>
2018-05-07 21:28:28 +0800

    add app routes

---
+++ src/components/routes.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Route, Switch,Redirect} from 'dva/router';
+
+const UnLogged = ()=>{
+  return <div>UnLogged</div>
+}
+const Logged = ()=>{
+  return <div>Logged</div>
+}
+
+export default class Routes extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div>
+          <Switch>
+            <Route path="/" exact component={UnLogged} />
+            <Route path="/home" exact component={UnLogged} />
+            <Route path="/wallet" render={Logged} />
+            <Route path="/trade" render={Logged} />
+          </Switch>
+      </div>
+    );
+  }
+}
+
+
+
+
