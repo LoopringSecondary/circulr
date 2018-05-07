13a0727c93265e06cda4b9c150a9e44afc78e639
houzongbao <houzongbao@126.com>
2018-05-07 22:38:08 +0800

    add ticker components

---
+++ src/components/tickers/ListAllTickers.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+function ListAllTickers(props) {
+  return (
+    <div>ListAllTickers</div>
+  )
+}
+export default ListAllTickers
---
+++ src/components/tickers/index.js
@@ -0,0 +1,11 @@
+import ListDefault from './ListDefault'
+import ListTradesHistory from './ListTradesHistory'
+import Detail from './Detail'
+import Modals from './Modals'
+
+export default {
+  ListDefault,
+  ListTradesHistory,
+  Detail,
+  Modals,
+}
