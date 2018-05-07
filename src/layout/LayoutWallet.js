3592d40badfae91b051dd5b415c94f983391ab55
houzongbao <houzongbao@126.com>
2018-05-07 23:36:34 +0800

    add layoyt components

---
+++ src/layout/LayoutWallet.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+function LayoutWallet(props) {
+  return (
+    <div>LayoutWallet</div>
+  )
+}
+export default LayoutWallet
---
+++ src/layout/index.js
@@ -0,0 +1,9 @@
+import LayoutHome from './LayoutHome'
+import LayoutWallet from './LayoutWallet'
+import LayoutTrade from './LayoutTrade'
+
+export default {
+	LayoutHome,
+	LayoutWallet,
+	LayoutTrade,
+}
