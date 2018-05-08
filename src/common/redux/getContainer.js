900c6e8c528fa1b5aa04ead84cde5af3bb34c375
houzongbao <houzongbao@126.com>
2018-05-08 11:30:37 +0800

    add getContainer

--- src/common/redux/getContainer.js
+++ src/common/redux/getContainer.js
@@ -1,6 +1,48 @@
-function getContainer() {
-
-
+import React, { Component } from 'react';
+import { connect } from 'dva';
+import { bindActionCreators } from 'redux';
+const getComponent = (namespace)=>{
+  return class Container extends React.Component {
+    render() {
+      const { children,dispatch,[namespace]:data,...rest} = this.props
+      const actions = bindActionCreators(actionCreators,dispatch)
+      const childProps = {
+        ...rest,
+        [namespace]:{
+          ...data,
+          ...actions,
+        }
+      }
+      return (
+        <div>
+          {
+            React.Children.map(this.props.children, child => {
+                return React.cloneElement(child, {...childProps})
+            })
+          }
+        </div>
+      )
+    }
+  }
 }
 
-export default getContainer;
+function getContainer(model,config) {
+  const namespace =  model.namespace
+  const reducersKeys = Object.keys(model.reducers)
+  const stateKeys = Object.keys(model.reducers)
+  const effectsKeys = Object.keys(model.effects)
+  let keys = [reducersKeys,reducersKeys]
+  keys = keys.map(key=>key.replace(`${namespace}/`,''))
+  const actionCreators = getActionCreators(namespace,keys)
+  const dispatch 
+  const actions = bindActionCreators(actionCreators,dispatch)
+  let path = ''
+  if(config && config.path){
+    // TODO flat nest 
+    path = config.path
+  }else{
+    path = namespace
+  }
+  return connect(({[path]:value})=>({[path]:value}))(getComponent(path))
+}
+export default getContainer
