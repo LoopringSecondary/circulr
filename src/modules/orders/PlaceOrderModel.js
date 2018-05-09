bbf8e4ef005a9e550ac75678fddbd5ec5161a5ec
dyfvicture <fbi-ceo@163.com>
2018-05-09 11:30:53 +0800

    trade modal

--- src/modules/orders/PlaceOrderModel.js
+++ src/modules/orders/PlaceOrderModel.js
@@ -2,8 +2,26 @@
 export default {
   namespace: 'placeOrder',
   state: {
-   sell:{},
-   buy:{},
+   sell:{
+     token:null
+   },
+   buy:{
+     token:null
+   },
+   trade:{
+     priceInput: 0,
+     amountInput:0,
+     availableAmount: 0,
+     timeToLivePatternSelect: 'easy',
+     timeToLivePopularSetting: true,
+     sliderMilliLrcFee:0,
+     timeToLive:0,
+     timeToLiveUnit:'',
+     timeToLiveStart: null,
+     timeToLiveEnd: null,
+     total:0,
+     loading: false,
+   }
   },
   effects:{
     // TODO
