56c6d70fd52d9e9d16b081039c4313c8ff26aecf
lyd <lyddream@126.com>
2018-05-09 18:04:20 +0800

    complete convert modules

--- src/modules/tokens/ConvertModel.js
+++ src/modules/tokens/ConvertModel.js
@@ -34,20 +34,49 @@ export default {
         ...state,
         amount
       }
+    },
+    setIsMax(state,{payload}){
+      const {isMax} = payload;
+      return {
+        ...state,
+        isMax
+      }
+    },
+    setGasPrice(state,{payload}){
+      const {gasPrice} = payload;
+      return {
+        ...state,
+        gasPrice
+      }
+    },
+    setToken(state,{payload}){
+      const {token} = payload;
+      return {
+        ...state,
+        token
+      }
     }
   },
   effects: {
-    * amoutChange({payload}, {select, put}) {
+    * amountChange({payload}, {select, put}) {
       const {amount} = payload;
-      const token = this.state.token;
+      const {token} = yield select((state) =>state.convert);
       const assets = yield select((state) => getAssetsByToken(state,token,true));
       const outBalance = assets.balance.lt(toBig(amount));
       yield put({type:"setAmount",payload:{amount}});
       yield put({type:"setOutBalance",payload:{outBalance}})
     },
-
-
-
+    *gasPriceChange({payload},{select,put}){
+      const {gasPrice} = payload;
+      const {token,isMax,gasLimit} = yield select((state) => state.convert);
+      if(isMax && token.toLowerCase() === 'eth'){
+        const assets = yield select((state) => getAssetsByToken(state,token));
+        const gas = toBig(gasPrice).times(gasLimit).times(1e9);
+        const amount = assets.balance.minus(gas).isPositive() ? assets.balance.minus(gas) : toBig(0);
+        yield put({type:'amountChange',payload:{amount}})
+      }
+     yield put({type:'setGasPrice',payload:{gasPrice}})
+    }
   },
 
 };
