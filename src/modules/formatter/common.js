7b2e492086fd9e7620ac4b19948e864195986f00
lyd <lyddream@126.com>
2018-05-09 11:49:49 +0800

    token formatter

--- src/modules/formatter/common.js
+++ src/modules/formatter/common.js
@@ -1,5 +1,5 @@
 import * as moment from 'moment';
-import {toNumber,toBig} from "LoopringJS/common/formatter";
+import {toBig, toFixed, toNumber} from "LoopringJS/common/formatter";
 import intl from 'react-intl-universal';
 
 export function getShortAddress(address) {
@@ -15,13 +15,13 @@ export function getFormatTime(seconds, style) {
   return moment(toNumber(seconds)).format(style);
 }
 
-export function getFormattedTime(timestamp='0',format="YYYY-MM-DD hh:mm:ss"){
-  return moment(timestamp,'X').format(format)
+export function getFormattedTime(timestamp = '0', format = "YYYY-MM-DD hh:mm:ss") {
+  return moment(timestamp, 'X').format(format)
 }
 
 export function getSeconds(value, unit) {
   value = Number(value);
-  switch(unit){
+  switch (unit) {
     case 'second':
       return value;
     case 'minute':
@@ -29,7 +29,7 @@ export function getSeconds(value, unit) {
     case 'hour':
       return value * 3600;
     case 'day':
-      return value * 3600 *24;
+      return value * 3600 * 24;
     default:
       return value;
   }
@@ -40,9 +40,33 @@ export function getFormatNum(number) {
   number = toBig(number).toString(10).split('.');
   let a = number[0];
   let b = number[1];
-  a = intl.get('global.amount',{amount:toNumber(a)});
+  a = intl.get('global.amount', {amount: toNumber(a)});
   const symbol = window.locale && window.locale.startsWith('fr') ? ',' : '.';
   return b ? a.concat(symbol).concat(b) : a
+}
+
+export function formatLength(value, ceil) {
+  value = Number(value)
+  // fix bug: value == string
+  if (value && typeof value === 'number') {
+  } else {
+    value = 0
+  }
+  if (value > 1000) {
+    return toFixed(value, 2, ceil)
+  }
+  if (value <= 1000 && value >= 1) {
+    return toFixed(value, 2, ceil)
+  }
+  if (value < 1 && value >= 0.001) {
+    return toFixed(value, 5, ceil)
+  }
+  if (value < 0.001 && value > 0) {
+    return toFixed(value, 8, ceil)
+  }
+  if (value === 0) {
+    return 0.00
+  }
 }
 
 
@@ -51,5 +75,6 @@ export default {
   getFormatTime,
   getFormattedTime,
   getSeconds,
-  getFormatNum
+  getFormatNum,
+  formatLength
 }
--- src/modules/tokens/formatters.js
+++ src/modules/tokens/formatters.js
@@ -1,31 +1,8 @@
-import {toNumber,toBig,toFixed} from "LoopringJS/common/formatter";
+import {toBig, toNumber,toFixed} from "LoopringJS/common/formatter";
+import {formatLength} from "../formatter/common";
 
 export function getPrice(){
   // TODO
-}
-
-const formatLength = (value)=>{
-  value =  Number(value)
-  // fix bug: value == string
-  if(value && typeof value === 'number'){
-  }else{
-     value = 0
-  }
-  if(value>1000){
-    return toFixed(value,2)
-  }
-  if(value<=1000 && value>=1){
-    return value.toFixed(2)
-  }
-  if(value<1 && value>=0.001){
-    return value.toFixed(5)
-  }
-  if(value<0.001 && value>0){
-    return value.toFixed(8)
-  }
-  if(value===0){
-    return 0.00
-  }
 }
 
 export default class TokenFormatter {
@@ -48,32 +25,33 @@ export default class TokenFormatter {
       })
   }
   getToken(){
-    let keys = Object.keys(this)
-    let token = {}
+    let keys = Object.keys(this);
+    let token = {};
     keys.forEach(key=>{
       token[key] = this[key]
-    })
+    });
     return token
   }
 
-  getAmount(amount){
-    let number
-    if(amount){
-      number = (toBig(amount).div('1e' + this.digits)).toFixed(this.precision)
-    }else{
-      number = 0
-    }
-    return formatLength(number)
+  getUnitAmount(amount){
+    amount = amount || 0;
+    return toBig(amount).div('1e' + this.digits)
+  }
+  getUnitAmountValue(amount,price){
+    const unitAmount = this.getUnitAmount(amount);
+    return unitAmount.times(toBig(price))
+  }
+  getUnitBalance(){
+    return this.getUnitAmount(this.balance)
+  }
+  getUnitBalanceValue(price){
+    return this.getUnitAmountValue(this.balance,price)
+  }
 
+  toPricisionFixed(amount,ceil){
+    toFixed(amount,this.precision,ceil)
   }
-  getAmountValue(amount,price){
-    const formattedAmount = this.getAmount(amount)
-    return formatLength(formattedAmount * price)
-  }
-  getBalance(){
-    return this.getAmount(this.balance)
-  }
-  getBalanceValue(price){
-    return this.getAmountValue(this.balance,price)
+  toFormatLength(amount,ceil){
+    formatLength(amount,ceil)
   }
 }
