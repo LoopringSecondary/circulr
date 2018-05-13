0c4f521bee49918c03a8e359dc35ca2bf43e64ba
houzongbao <houzongbao@126.com>
2018-05-13 09:41:39 +0800

    update orders formatter

--- src/modules/orders/formatters.js
+++ src/modules/orders/formatters.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import {Icon} from 'antd';
 import {toNumber,toBig} from "LoopringJS/common/formatter";
+import config from "common/config";
+import commonFm from "../formatter/common";
 import intl from 'react-intl-universal';
-const getFormatTime = ()=>{}
 
 const status = {
   ORDER_NEW: {},
@@ -11,47 +12,73 @@ const status = {
   ORDER_CANCEL: {},
   ORDER_CUTOFF: {}
 }
-export default const formatters = {
-  getOrderHash:(item)=>item.originalOrder.hash,
-  getMarket: (item) =>`${item.originalOrder.tokenB}/${item.originalOrder.tokenS}`,
-  getSide: (item) =>item.side,
-  getAmount: (item) => {
-    const side = item.originalOrder.side.toLowerCase();
-    let token =  side === 'buy' ? window.CONFIG.getTokenBySymbol(item.originalOrder.tokenB) : window.CONFIG.getTokenBySymbol(item.originalOrder.tokenS);
+export class OrdersFm {
+  constructor(order){
+    this.order = order
+  }
+  getOrderHash(){return this.order.originalOrder.hash}
+  getMarket(){return `${this.order.originalOrder.tokenB}/${this.order.originalOrder.tokenS}`}
+  getSide(){return this.order.sidem }
+  getAmount(){
+    const side = this.order.originalOrder.side.toLowerCase();
+    let token =  side === 'buy' ? config.getTokenBySymbol(this.order.originalOrder.tokenB) : config.getTokenBySymbol(this.order.originalOrder.tokenS);
     token = token || {digits: 18, precision: 6};
-    const amount = side === 'buy' ? item.originalOrder.amountB : item.originalOrder.amountS;
-    const symbol = side === 'buy' ? item.originalOrder.tokenB : item.originalOrder.tokenS;
-    return window.uiFormatter.getFormatNum(toNumber((toNumber(amount) / Number('1e' + token.digits)).toFixed(token.precision))) + ' ' + symbol
-  },
-  getPrice: (item) => {
-    const tokenB = window.CONFIG.getTokenBySymbol(item.originalOrder.tokenB);
-    const tokenS = window.CONFIG.getTokenBySymbol(item.originalOrder.tokenS);
-    const market = window.CONFIG.getMarketBySymbol(item.originalOrder.tokenB,item.originalOrder.tokenS);
-    const price =  item.originalOrder.side.toLowerCase() === 'buy' ?
-      toBig(item.originalOrder.amountS).div('1e'+tokenS.digits).div(toBig(item.originalOrder.amountB).div('1e'+tokenB.digits)).toFixed(market.pricePrecision) :
-      toBig(item.originalOrder.amountB).div('1e'+tokenB.digits).div(toBig(item.originalOrder.amountS).div('1e'+tokenS.digits)).toFixed(market.pricePrecision);
-    return window.uiFormatter.getFormatNum(price)
-  },
-  getTotal: (item) => {
-      const side = item.originalOrder.side.toLowerCase();
-      const tokenS = item.originalOrder.tokenS;
-      const tokenB = item.originalOrder.tokenB;
-      const amountS = item.originalOrder.amountS;
-      const amountB = item.originalOrder.amountB;
-      let token = side === 'buy' ? window.CONFIG.getTokenBySymbol(tokenS): window.CONFIG.getTokenBySymbol(tokenB);
+    const amount = side === 'buy' ? this.order.originalOrder.amountB : this.order.originalOrder.amountS;
+    const symbol = side === 'buy' ? this.order.originalOrder.tokenB : this.order.originalOrder.tokenS;
+    return commonFm.getFormatNum(toNumber((toNumber(amount) / Number('1e' + token.digits)).toFixed(token.precision))) + ' ' + symbol
+  }
+  getPrice(){
+    const tokenB = config.getTokenBySymbol(this.order.originalOrder.tokenB);
+    const tokenS = config.getTokenBySymbol(this.order.originalOrder.tokenS);
+    const market = config.getMarketBySymbol(this.order.originalOrder.tokenB,this.order.originalOrder.tokenS);
+    const price =  this.order.originalOrder.side.toLowerCase() === 'buy' ?
+      toBig(this.order.originalOrder.amountS).div('1e'+tokenS.digits).div(toBig(this.order.originalOrder.amountB).div('1e'+tokenB.digits)).toFixed(market.pricePrecision) :
+      toBig(this.order.originalOrder.amountB).div('1e'+tokenB.digits).div(toBig(this.order.originalOrder.amountS).div('1e'+tokenS.digits)).toFixed(market.pricePrecision);
+    return commonFm.getFormatNum(price)
+  }
+  getTotal(){
+      const side = this.order.originalOrder.side.toLowerCase();
+      const tokenS = this.order.originalOrder.tokenS;
+      const tokenB = this.order.originalOrder.tokenB;
+      const amountS = this.order.originalOrder.amountS;
+      const amountB = this.order.originalOrder.amountB;
+      let token = side === 'buy' ? config.getTokenBySymbol(tokenS): config.getTokenBySymbol(tokenB);
       token = token || {digits: 18, precision: 6};
       const amount = side === 'buy' ? amountS : amountB;
       const symbol = side === 'buy' ? tokenS : tokenB;
       const total = (toNumber(amount) / Number('1e' + token.digits)).toFixed(token.precision)
-      return  window.uiFormatter.getFormatNum(toNumber(total)) + ' ' +symbol
-  },
-  getLrcFee: (item) => {
-      let token = window.CONFIG.getTokenBySymbol('LRC');
+      return  commonFm.getFormatNum(toNumber(total)) + ' ' +symbol
+  }
+  getLrcFee(){
+      let token = config.getTokenBySymbol('LRC');
       token = token || {digits: 18, precision: 6};
-      const total = (toNumber(item.originalOrder.lrcFee) / Number('1e' + token.digits)).toFixed(token.precision);
-      return window.uiFormatter.getFormatNum(toNumber(total))  + ' LRC'
-  },
-  getTimestamp: (item) => getFormatTime(toNumber(item.originalOrder.validSince) * 1e3),
-  getFilled:()=>{},
-  getStatus:()=>item.status,
+      const total = (toNumber(this.order.originalOrder.lrcFee) / Number('1e' + token.digits)).toFixed(token.precision);
+      return commonFm.getFormatNum(toNumber(total))  + ' LRC'
+  }
+  getTimestamp(){return commonFm.getFormatTime(toNumber(this.order.originalOrder.validSince) * 1e3)}
+  getFilled(){}
+  getStatus(){return this.order.status}
+}
+export class PlaceOrderFm {
+  constructor(input){
+    this.input = input
+  }
+  getPrice(){
+
+  }
+  getAmount(){
+
+  }
+  getAvailableAmount(){
+
+  }
+  getTotal(){
+
+  }
+  getTTL(){
+
+  }
+  getLRCFee(){
+
+  }
 }
