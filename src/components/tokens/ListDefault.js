ccd00f6d4167fcadf57c6e398205c4eafe7498fd
houzongbao <houzongbao@126.com>
2018-05-15 07:45:12 +0800

    update tokens

--- src/components/tokens/ListDefault.js
+++ src/components/tokens/ListDefault.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import {Input,Icon,Tooltip} from 'antd'
+import intl from 'react-intl-universal'
 
 function ListHeaderForm({className=''}){
   return (
@@ -17,26 +19,124 @@ function ListHeader({className=''}){
 }
 
 function ListTokensSidebar(props) {
-  const {tokens}= props
+  const {tokens:list,dispatch}= props
+  const {filters,favored,selected}= list
+  const toggleMyFavorite = () => {
+      list.filtersChange({
+        filters: {
+          ...filters,
+          ifOnlyShowMyFavorite: !filters.ifOnlyShowMyFavorite
+        }
+      })
+    }
+    const toggleSmallBalance = () => {
+      list.filtersChange({
+        filters: {
+          ...filters,
+          ifHideSmallBalance: !filters.ifHideSmallBalance
+        }
+      })
+    }
+    const searchToken = (e) => {
+      list.filtersChange({
+        filters: {
+          ...filters,
+          keywords: e.target.value
+        }
+      })
+    }
+    const toggleFavored = (item, e) => {
+      e.stopPropagation()
+      list.favoredChange({
+        favored: {
+          [item.symbol]: !favored[item.symbol], // TODO address
+        }
+      })
+    }
+    const toggleSelected = (item) => {
+      let new_selected = {}
+      for (let key in selected) {
+        new_selected[key] = false
+      }
+      list.selectedChange({
+        selected: {
+          ...new_selected,
+          [item.symbol]: true, // TODO address
+        }
+      })
+      updateTransations(item.symbol)
+    }
+    const updateTransations = (token) => {
+      dispatch({
+        type: 'transactions/filtersChange',
+        payload: {
+          filters: {token}
+        }
+      })
+    }
   return (
     <div>
     	<div className="token-total">
           <h3 className="text-success">$39,484,950</h3><small>Total Value</small>
       </div>
       <div className="tool-bar d-flex justify-content-between">
-          <div className="favorites active"><i className="icon-star-o"></i></div>
+          <div className="favorites"><i className="icon-star"></i></div>
           <div className="token-view"><i className="icon-eye-o"></i></div>
           <div className="search"><i className="icon-search"></i>
               <input id="icon" />
           </div>
+          <Input
+            placeholder=""
+            suffix={<Icon type="search" className="color-grey-600"/>}
+            prefix={
+              <div className="row gutter-0 align-items-center">
+                <div className="col"></div>
+                <div className="col-auto pr5">
+                  <Tooltip title={intl.get('tokens.only_show_favorites')}>
+                    {
+                      filters.ifOnlyShowMyFavorite &&
+                      <Icon type="star" onClick={toggleMyFavorite.bind(this)}
+                            className="fs16 color-primary-1 border-none pointer"
+                      />
+                    }
+                    {
+                      !filters.ifOnlyShowMyFavorite &&
+                      <Icon onClick={toggleMyFavorite.bind(this)} className="fs16 color-black-2 pointer"
+                            type="star-o"/>
+                    }
+                  </Tooltip>
+                </div>
+                <div className="col-auto zb-b-l pl5">
+                  <Tooltip title={intl.get('tokens.hide_small_balances')}>
+                    {
+                      filters.ifHideSmallBalance &&
+                      <Icon onClick={toggleSmallBalance.bind(this)} className="fs18 color-primary-1 pointer"
+                            style={{position: 'relative', marginTop: '2px'}}
+                            type="eye"/>
+                    }
+                    {
+                      !filters.ifHideSmallBalance &&
+                      <Icon onClick={toggleSmallBalance.bind(this)} className="fs18 color-black-2 pointer"
+                            style={{position: 'relative', marginTop: '2px'}}
+                            type="eye-o"/>
+                    }
+                  </Tooltip>
+                </div>
+              </div>
+            }
+            className="d-block w-100"
+            onChange={searchToken.bind(this)}
+            value={filters.keywords}
+            addonAfter={null}
+          />
       </div>
       <div className="token-list text-color-dark-1" style={{height: "100%",overflow: "auto",paddingBottom: "211px"}}>
           <div className="">
               {
-                tokens.items.map((item,index)=>
-                  <div className="item" key={index}>
+                list.items.map((item,index)=>
+                  <div className="item" key={index} onClick={()=>{}}>
                       <div className="sub">
-                          <div className="favorites"><i className="icon-star"></i></div>
+                          <div hidden className="favorites"><i className="icon-star"></i></div>
                           <div className="icon"><i className={`icon-${item.symbol} icon-token`}></i></div>
                           <div className="token-name">
                               <h3>{item.symbol}</h3>
