450bcf3808362c1a2590bcfa901c4e5e839f3858
changshuhong <changshuhong@163.com>
2018-05-14 18:08:51 +0800

    修改样式

--- src/components/orders/PlaceOrderConfirm.js
+++ src/components/orders/PlaceOrderConfirm.js
@@ -1,8 +1,33 @@
 import React from 'react';
-
+import { Input,Button } from 'antd';
 function PlaceOrderConfirm(props) {
   return (
-    <div>PlaceOrderConfirm</div>
+    <div>
+        <div class="pd-lg text-center">
+	        <em>您正在出售</em>
+	        <h3>100LRC</h3>
+	        <small>0.0015 × 100 =0.15WETH</small>
+        </div>
+        <div class="divider solid"></div>
+        <ul class="list list-label hover list-justify-flex-start divided">
+            <li><span>撮合费</span><span>0.5LRC</span></li>
+            <li><span>分润比例</span><span>50%</span></li>
+            <li><span>订单生效时间</span><span>2018年5月14日 16:28</span></li>
+            <li><span>订单失效时间</span><span>2018年5月15日 16:28</span></li>
+        </ul>
+        <div><i class="icon-chevron-up"></i>签名信息</div>
+        <div class="col2-2">
+        	<div class="item">
+        	    <p>未签名的订单</p>
+        		<Input.TextArea placeholder="" autosize={{ minRows: 2, maxRows: 6 }} />
+        	</div>
+        	<div class="item">
+        	    <p>签名的订单</p>
+        		<Input.TextArea placeholder="" autosize={{ minRows: 2, maxRows: 6 }} />
+        	</div>
+        </div> 
+        <div class="text-center"></div>	                      
+    </div>
   )
 }
 export default PlaceOrderConfirm
