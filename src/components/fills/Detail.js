a602ba2bcd5b5b7207cc8904a3a2df45538469df
houzongbao <houzongbao@126.com>
2018-05-19 19:35:27 +0800

    update fills formatter

--- src/components/fills/Detail.js
+++ src/components/fills/Detail.js
@@ -4,7 +4,7 @@ import {Link} from 'dva/router'
 import CoinIcon from 'LoopringUI/components/CoinIcon'
 import {toNumber,toBig} from "LoopringJS/common/formatter";
 import intl from 'react-intl-universal';
-import {renders} from './ListMyOrders';
+import {renders} from './ListMyFills';
 import {FillFm} from 'modules/orders/formatters';
 
 const MetaItem = (props) => {
@@ -25,7 +25,7 @@ function OrderDetail(props) {
   const fillFm = new FillFm(fill)
    return (
     <div>
-        <div className="modal-header text-dark"><h3>成交详情</h3></div>
+        <div className="modal-header text-dark"><h3>环路详情</h3></div>
           <ul className="list list-label list-dark list-justify-space-between divided">
               <MetaItem label="环路" value={fillFm.getRingIndex()} />
               <MetaItem label="环路哈希" value={fillFm.getRingHash()} />
@@ -38,12 +38,10 @@ function OrderDetail(props) {
                   <span>环路哈希</span>
                   <div className="text-lg-control break-word text-right">0x58a2f1a15d97c25917e672000d80cf68b74ca192bf5542623de832918b1bba9b</div>
               </li>
-
               <li>
                     <span>矿工</span>
                   <div className="text-lg-control break-word text-right">0x3ACDF3e3D8eC52a768083f718e763727b02106</div>
               </li>
-
               <li><span>费用接收地址</span><div className="text-lg-control break-word text-right">0x3ACDF3e3D8eC52a768083f718e763727b02106</div></li>
               <li><span>总共的LRC费用</span><span>0.165617 LRC</span></li>
               <li><span>总分的分润费用</span><span>0.36 LRC</span></li>
--- src/modules/fills/formatters.js
+++ src/modules/fills/formatters.js
@@ -36,11 +36,39 @@ export class FillFm{
   getTotalSplitFee(){
     return 'TODO'
   }
-  getCreateTime(){
-    return commonFm.getFormatTime(toNumber(this.ring.timestamp) * 1e3)
-  }
   getTradeAmount(){
     return commonFm.getFormatNum(this.ring.tradeAmount)
+  }
+  getAmount(){
+    const fmS = this.fill.side.toLowerCase() === 'buy' ? new TokenFm({symbol: this.fill.tokenB}) : new TokenFm({symbol: this.fill.tokenS});
+    const amount = this.fill.side.toLowerCase() === 'buy' ? fmS.getUnitAmount(this.fill.amountB) : fmS.getUnitAmount(this.fill.amountS);
+    const symbol = this.fill.side === 'buy' ? this.fill.tokenB : this.fill.tokenS
+    return commonFm.getFormatNum(amount) + '' + symbol
+  }
+  getTotal(){
+    const fmS = this.fill.side.toLowerCase() === 'buy' ? new TokenFm({symbol: this.fill.tokenS}) : new TokenFm({symbol: this.fill.tokenB});
+    const amount = this.fill.side.toLowerCase() === 'buy' ? fmS.getUnitAmount(this.fill.amountS) : fmS.getUnitAmount(this.fill.amountB);
+    const symbol = this.fill.side === 'buy' ? this.fill.tokenS : this.fill.tokenB
+    return commonFm.getFormatNum(amount) + '' + symbol
+  }
+  getPrice(){
+    const tokenB = config.getTokenBySymbol(this.fill.tokenB);
+    const tokenS = config.getTokenBySymbol(this.fill.tokenS);
+    const market = config.getMarketByPair(this.fill.market);
+    const price = this.fill.side.toLowerCase() === 'buy' ? (toBig(this.fill.amountS).div('1e' + tokenS.digits).div(toBig(this.fill.amountB).div('1e' + tokenB.digits))).toFixed(market.pricePrecision) :
+      (toBig(this.fill.amountB).div('1e' + tokenB.digits).div(toBig(this.fill.amountS).div('1e' + tokenS.digits))).toFixed(market.pricePrecision);
+    return commonFm.getFormatNum(price)
+  }
+  getLRCFee(){
+    const fmLrc = new TokenFm({symbol: 'LRC'});
+    return commonFm.getFormatNum(fmLrc.getUnitAmount(this.fill.lrcFee)) + ' LRC'
+  }
+  getLRCReward(){
+    const fmLrc = new TokenFm({symbol: 'LRC'});
+    return commonFm.getFormatNum(fmLrc.getUnitAmount(this.fill.lrcReward)) + ' LRC'
+  }
+  getTime(){
+    return commonFm.getFormatTime(toNumber(this.fill.createTime) * 1e3)
   }
 }
 
