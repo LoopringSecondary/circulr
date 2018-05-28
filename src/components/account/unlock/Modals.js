3205e3b3b2f2ec93a029304f4dd25002dafbfd26
dyfvicture <fbi-ceo@163.com>
2018-05-28 15:07:40 +0800

    unlock modal

---
+++ src/components/account/unlock/Modals.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import {Containers} from 'modules'
+import UiContianers from 'LoopringUI/containers'
+import UnlockWallet from '../../account/unlock/UnlockWallet'
+function Modals(props) {
+  return (
+    <div>
+      <Containers.Layers id="unlock">
+        <UiContianers.Modals id="unlock">
+          <UnlockWallet />
+        </UiContianers.Modals>
+      </Containers.Layers>
+    </div>
+  )
+}
+export default Modals
