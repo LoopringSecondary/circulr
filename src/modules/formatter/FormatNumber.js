1594c1033616501a4666cbc1063cb1ddad61a3fe
dyfvicture <fbi-ceo@163.com>
2018-05-28 13:49:20 +0800

    place order format

--- src/modules/formatter/FormatNumber.js
+++ src/modules/formatter/FormatNumber.js
@@ -3,25 +3,25 @@ import {toBig, toFixed} from 'LoopringJS/common/formatter'
 import {getFormatNum} from './common'
 import { Button, Tooltip } from 'antd';
 
-export const FormatAmount = ({value, precision})=>{
+export const FormatAmount = ({value, precision = 4, tooptip = true})=>{
 
   const format = (value) => {
-    if(isNaN(value)) return value;
+    if(isNaN(value)) return {d:0, o:0, s:false};
     const x = toBig(value)
     if(x.lt(9999)) {
       return {d:getFormatNum(toFixed(x, precision)), o:x.toString(10), s:false};
     }
     if(x.lt(1000000)) {
       const shortNumber = toFixed(x.div(1000), precision)
-      return {d:`${getFormatNum(shortNumber)} K`, o:x.toString(10), s:true};
+      return {d:`${getFormatNum(shortNumber)}K`, o:x.toString(10), s:true};
     }
     if(x.lt(10000000)) {
       const shortNumber = toFixed(x.div(1000000), precision)
-      return {d:`${getFormatNum(shortNumber)} M`, o:x.toString(10), s:true};
+      return {d:`${getFormatNum(shortNumber)}M`, o:x.toString(10), s:true};
     }
     if(x.lt(1000000000000)) {
       const shortNumber = toFixed(x.div(1000000000), precision)
-      return {d:`${getFormatNum(shortNumber)} B`, o:x.toString(10), s:true};
+      return {d:`${getFormatNum(shortNumber)}B`, o:x.toString(10), s:true};
     }
     return {d:"1T+", o:x.toString(10), s:true};
   }
@@ -30,9 +30,14 @@ export const FormatAmount = ({value, precision})=>{
 
   return (
     <span>
+      {tooptip &&
       <Tooltip placement="top" title={formatted.o}>
         {formatted.d}
       </Tooltip>
+      }
+      {!tooptip &&
+        formatted.d
+      }
     </span>
   )
 }
