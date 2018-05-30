aa62a2b7c12d4bcde6b0d002c83d3a37a136a8ec
dyfvicture <fbi-ceo@163.com>
2018-05-30 13:52:45 +0800

    place order

--- src/components/orders/PlaceOrderSign.js
+++ src/components/orders/PlaceOrderSign.js
@@ -3,80 +3,120 @@ import {Button, Form, Input, Select, Slider,Card,Icon,Radio,Tabs,Steps,Collapse}
 import Alert from 'LoopringUI/components/Alert'
 import intl from 'react-intl-universal'
 import {connect} from 'dva'
+import Notification from 'LoopringUI/components/Notification'
 
-const PlaceOrderSign = ({
-    form
-  }) => {
+const PlaceOrderSign = (props) => {
+  const {placeOrder, wallet, dispatch} = props
+  const {signed, unsigned} = placeOrder
+  const isUnlocked =  wallet.address && wallet.unlockType && wallet.unlockType !== 'locked' && wallet.unlockType !== 'address'
+  const actualSigned = signed ? signed.filter(item => item !== undefined) : []
+  console.log(1111, signed, unsigned)
+
+  async function sign(item, index, e) {
+    e.preventDefault()
+    const account = wallet.account || window.account
+    if(!account || wallet.unlockType === 'address') {
+      Notification.open({
+        message: intl.get('trade.place_order_failed'),
+        type: "error",
+        description: 'to unlock'
+      });
+      return
+    }
+    try {
+      console.log(item.address, wallet.address)
+      if(item.address !== wallet.address) {
+        Notification.open({
+          message: intl.get('trade.place_order_failed'),
+          type: "error",
+          description: 'your address in original order is not the same as unlocked, please replace order'
+        });
+        return
+      }
+      if(item.type === 'order') {
+        const signedOrder = await account.signOrder(item.data)
+        signedOrder.powNonce = 100;
+        signed[index] = {type: 'order', data:signedOrder};
+      } else {
+        signed[index] = {type: 'tx', data: await account.signEthereumTx(item.data)};
+      }
+      dispatch({type:'placeOrder/signedChange',payload:{signed}})
+    } catch(e) {
+      console.error(e)
+      Notification.open({
+        message: intl.get('trade.place_order_failed'),
+        type: "error",
+        description: e.message
+      });
+    }
+  }
+
   const TxHeader = ({tx,index})=>{
     return (
       <div className="row pl0 pr0 pt10 pb10 align-items-center">
         <div className="col">
           <div className="fs18 color-black-1">
             <Button type="primary" shape="circle" className="mr10">{index+1}</Button>
-            {tx.title}
+            {tx.description}
           </div>
         </div>
         <div className="col-auto pr20">
-          {tx.isSigned &&
+          {signed[index] &&
             <div className="text-up">
                Signed <Icon className="ml5" type="check-circle"  />
             </div>
           }
-          {!tx.isSigned &&
+          {!signed[index] &&
             <div className="color-black-3">
-              <a href="">Sign<Icon className="ml5" type="right"  /></a>
+              <a onClick={sign.bind(this, tx, index)}>Sign<Icon className="ml5" type="right"  /></a>
             </div>
           }
         </div>
       </div>
     )
   }
-  const TxContent = ({tx,key})=>{
+  const TxContent = ({tx,index})=>{
     return (
       <div className="row pl0 pr0 ">
         <div className="col-6 pr5">
-          <Input.TextArea placeholder="" autosize={{ minRows: 3, maxRows: 6 }} value={""}/>
+          <Input.TextArea placeholder="" autosize={{ minRows: 3, maxRows: 6 }} value={JSON.stringify(unsigned[index])}/>
         </div>
         <div className="col-6 pl5">
-          <Input.TextArea placeholder="" autosize={{ minRows: 3, maxRows: 6 }} value={""}/>
+          <Input.TextArea placeholder="" autosize={{ minRows: 3, maxRows: 6 }} value={signed && signed[index] ? JSON.stringify(signed[index]) : ''}/>
         </div>
       </div>
     )
   }
-  const txs = [
-    {
-      title:'Sign tx to enable LRC',
-      isSigned:true,
-    },
-    {
-      title:'Sign tx to enable EOS',
-      isSigned:false,
-    },
-    {
-      title:'Sign tx to submit order',
-      isSigned:false,
-    },
-  ]
+
   return (
     <div>
       <div className="pb10 fs18 color-black-1 zb-b-b">交易签名</div>
 
       <div className="mb15"></div>
-      <Alert type="info" title="您需要通过 Metamask 完成下面 3 个交易的签名：" theme="light" size="small" />
+      <Alert type="info" title={`您需要通过 ${wallet.unlockType} 完成下面 ${unsigned.length} 个交易的签名：`} theme="light" size="small" />
       <div className="mb15"></div>
       <Collapse defaultActiveKey={[]}>
         {
-          txs.map((item,index)=>
-            <Collapse.Panel header={<TxHeader tx={item} index={index} />} key={index} showArrow={false}>
-              <TxContent tx={item} />
-            </Collapse.Panel>
-          )
+          isUnlocked && unsigned.map((item, index)=>{
+            return (
+              <Collapse.Panel header={<TxHeader tx={item} index={index} />} key={index} showArrow={false}>
+                <TxContent tx={item} index={index}/>
+              </Collapse.Panel>
+            )
+          })
         }
       </Collapse>
     </div>
   );
 };
 
-export default Form.create()(connect()(PlaceOrderSign));
+function mapToProps(state) {
+  return {
+    placeOrder:state.placeOrder,
+    wallet:state.wallet
+  }
+}
+
+export default Form.create()(connect(mapToProps)(PlaceOrderSign));
 
 
