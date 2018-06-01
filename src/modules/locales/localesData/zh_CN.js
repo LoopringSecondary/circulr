30cba30288e4bff326d7e381e97ed8754aa4fb2e
lyd <lyddream@126.com>
2018-06-01 12:06:49 +0800

    fix wallet locales

--- src/modules/locales/localesData/zh_CN.js
+++ src/modules/locales/localesData/zh_CN.js
@@ -36,16 +36,17 @@ const words = {
   copy: "复制",
   copy_suc: '复制成功',
   copy_fail: "复制失败",
-  token:'代币',
-  order_type:'订单类型',
+  token: '代币',
+  order_type: '订单类型',
   margin_split: "分润",
   order_since: "订单生效时间",
   order_until: "订单失效时间",
   format_amount: "{amount,number}",
-  back:'返回',
-  previous_page:'前一页',
-  next_page:'后一页',
-  import:"导入"
+  back: '返回',
+  cancel:"取消",
+  previous_page: '前一页',
+  next_page: '后一页',
+  import: "导入"
 }
 const types = {
   trade_side: {
@@ -59,17 +60,19 @@ const validation_messages = {
 }
 
 const notifications = {
-  title:{
+  title: {
     place_order_failed: "订单提交失败 !",
     place_order_success: "下单成功!",
     place_order_warn: '您的订单只能被部分撮合',
+    unlock_suc: '解锁成功',
+    unlock_fail: "解锁失败",
   },
-  message:{
+  message: {
     wallet_locked: '您的钱包还未解锁，请先解锁后再继续操作',
     failed_fetch_data_from_server: '从服务器获取数据失败, 请稍后在尝试',
     eth_is_required_when_place_order: '由于需要支付ETH油费, 根据您当前订单需要发送的以太坊交易计算，还需要 {required} ETH',
     lrcfee_is_required_when_place_order: '由于需要支付LRC油费, 汇总您历史订单所需LRC，还需要 {required} LRC',
-    some_items_not_signed:"您可能还有一些数据还未签名，请把所有未签名项签名后再继续操作",
+    some_items_not_signed: "您可能还有一些数据还未签名，请把所有未签名项签名后再继续操作",
     place_order_success: '恭喜, 您的订单已经可以等待交易',
     place_order_balance_not_enough: '为使订单全部成交, 至少还需要{amount} {token}',
   }
@@ -91,7 +94,7 @@ const time_unit = {
 }
 
 export default {
-  common:{
+  common: {
     ...words,
     ...validation_messages,
     ...time_unit,
@@ -114,9 +117,9 @@ export default {
     expired: '过期时间',
     status: words.total,
   },
-  order_type:{
-    market_order : '公开市场订单',
-    p2p_order : '私密点对点订单'
+  order_type: {
+    market_order: '公开市场订单',
+    p2p_order: '私密点对点订单'
   },
   order_status: {
     open: '撮合中',
@@ -136,9 +139,7 @@ export default {
     tabs_basic: '基础信息',
     tabs_fill: '成交信息',
   },
-  place_order: {
-
-  },
+  place_order: {},
   gas_setting: {
     // TODO
   },
@@ -149,7 +150,7 @@ export default {
     // TODO
   },
   place_order_confirm: {
-    qrcode_security:'*为了您订单的安全，二维码只会生成一次并且不会保存在任何地方。请确认妥善保存二维码，任何收到您二维码的人都有可能吃掉您的订单。'
+    qrcode_security: '*为了您订单的安全，二维码只会生成一次并且不会保存在任何地方。请确认妥善保存二维码，任何收到您二维码的人都有可能吃掉您的订单。'
   },
   p2p_order: {
     order_title: '私密点对点交易',
@@ -158,11 +159,11 @@ export default {
     token_balance: '代币余额',
     order_detail: '订单详情',
     generate_order: '生成订单',
-    instruction:'1. 以您希望的兑换率生成一个订单，把不包含鉴权数据（没有这部分数据任何人都无法撮合您的订单）的订单信息提交给relay，同时将生成的订单hash和鉴权信息生成二维码。</br>2. 您可以把这个二维码发送给您的朋友，任何人拿到这个二维码都有可能吃掉您的订单，请注意以安全的方式传播。</br>3. 对方使用Circulr移动端扫描二维码，下一个与您买入卖出量完全匹配的对手单，发送以太坊交易吃掉这个订单，因此吃单方需要消耗油费。',
+    instruction: '1. 以您希望的兑换率生成一个订单，把不包含鉴权数据（没有这部分数据任何人都无法撮合您的订单）的订单信息提交给relay，同时将生成的订单hash和鉴权信息生成二维码。</br>2. 您可以把这个二维码发送给您的朋友，任何人拿到这个二维码都有可能吃掉您的订单，请注意以安全的方式传播。</br>3. 对方使用Circulr移动端扫描二维码，下一个与您买入卖出量完全匹配的对手单，发送以太坊交易吃掉这个订单，因此吃单方需要消耗油费。',
     notice: '* P2P订单双方都不需要支付LRC手续费</br>'
   },
   sign: {
-    not_signed : "您还未完成签名",
+    not_signed: "您还未完成签名",
     to_sign: "去签名"
   },
   // -----------
@@ -229,75 +230,95 @@ export default {
   },
   transfer: {},
   convert: {},
-  unlock:{
+  unlock: {
     has_not_unlocked: '您的钱包还未解锁',
-    to_unlock:'解锁钱包'
+    to_unlock: '解锁钱包',
+    title_connect: '连接{walletType}',
+    actions_unlock: '解锁',
+    actions_connect: "连接您的{walletType}",
+    connect_ledger_tip: "请连接您的Ledger钱包",
+    connect_trezor_tip: "请连接您的TREZOR",
+    error_invalid_tip: "信息不合法",
   },
-  wallet: {
-    types: {
-      generate: '生成钱包',
-      metamask: "MetaMask 钱包",
-      json: "Json 文件",
-      mnemonic: '助记词',
-      private_key: '私钥',
-      trezor: 'TREZOR',
-      ledger: 'Ledger 钱包',
-    },
-    title_generate: '生成钱包',
-    password_strength_title:'密码强度',
+  wallet_type:{
+    generate: '生成钱包',
+    metamask: "MetaMask 钱包",
+    json: "Json 文件",
+    mnemonic: '助记词',
+    private_key: '私钥',
+    trezor: 'TREZOR',
+    ledger: 'Ledger 钱包'
+  },
+
+  password:{
+    password_strength_title: '密码强度',
     password_strength: {
       weak: '弱',
       average: '一般',
       strong: '强'
     },
-    password_tips_weak:'密码强度不足,至少需要7个字符。',
-    password_tips_lack:"请输入密码",
-    backup_title:'备份钱包',
-    backup_tip: 'Circular钱包不会保存用户的私钥、Keystore、助记词，强烈建议您在线下备份这些信息（不联网的USB硬盘或纸质存储）。一旦私钥、Keystore、助记词丢失将无法恢复!',
-    default_address: '默认地址',
-    paste_address_title:'请粘贴您的地址',
-    paste_private_title:"请粘贴您的私钥",
-    title_json:'选择JSON文件',
-    title_deter_address:"选择地址",
-    title_select_path:'选择dpath',
-    custom_path:'自定义dpath',
-    actions_backup_json:'我已经明白，下载钱包文件',
-    actions_backup_mnemonic:'我已经明白，复制助记词',
-    actions_backup_private:'我已经明白，复制私钥',
-    actions_unlock: '解锁',
-    actions_generate: '生成钱包',
-    actions_other_address: '选择其他地址',
-    actions_get_metamask: "下载MetaMask插件",
-    actions_visit_metaMask: "访问MetaMask官网",
-    actions_connect: "连接您的{walletType}",
-    title_connect:'连接{walletType}',
-    actions_select_json: '选择JSON文件',
-    actions_paste_mnemonic: '请粘贴您的助记词',
-    error_json_tip: '无效的Keystore Json',
-    error_mnemonic_tip: "无效的助记词",
-    error_address_tip:'不合法的地址',
-    error_private_tip:'不合法的私钥',
-    error_invalid_tip:"信息不合法",
-    browser_tip:'您的浏览器不支持MetaMask解锁，推荐您使用Chrome',
-    unlock_metaMask_tip:'解锁MetaMask',
-    install_metaMask_tip:'安装MetaMask',
-    metamask_unlock_steps_title: '你要做的步骤',
-    metamask_unlock_refresh_button: '全做完了？刷新Circular',
-    metamask_unlock_cancel_button: '取消',
-    metamask_unlock_step_install_title: '安装MetaMask',
-    metamask_unlock_step_install_content: '为你的浏览器安装MetaMask插件',
-    metamask_unlock_step_unlock_title: '解锁MetaMask',
-    metamask_unlock_step_unlock_content: '在MetaMask上创建一个账号或解锁已有账号',
-    metamask_unlock_step_refresh_title: '刷新Circular',
-    metamask_unlock_step_refresh_content: '刷新Circular钱包使MetaMask生效',
-    connect_ledger_tip:"请连接您的Ledger钱包",
-    connect_trezor_tip:"请连接您的TREZOR",
-    no_address_tip:'没有合法的地址',
-    mnemonic_tip_lack:"请输入您的助记词",
-    error_password_tip: "请输入密码",
-    notification_unlock_suc: '解锁成功',
-    notification_unlock_fail: "解锁失败",
+    password_tips_weak: '密码强度不足,至少需要7个字符。',
+    password_tips_lack: "请输入密码"
   },
+
+  wallet_generate:{
+    title_generate: '生成钱包',
+    backup_title: '备份钱包',
+    backup_tip: 'Circular钱包不会保存用户的私钥、Keystore、助记词，强烈建议您在线下备份这些信息（不联网的USB硬盘或纸质存储）。一旦私钥、Keystore、助记词丢失将无法恢复!',
+    actions_backup_json: '我已经明白，下载钱包文件',
+    actions_backup_mnemonic: '我已经明白，复制助记词',
+    actions_backup_private: '我已经明白，复制私钥',
+    actions_generate: '生成钱包',
+  },
+
+  wallet_meta:{
+    actions_get_metaMask: "下载MetaMask插件",
+    actions_visit_metaMask: "访问MetaMask官网",
+    browser_tip: '您的浏览器不支持MetaMask解锁，推荐您使用Chrome',
+    unlock_metaMask_tip: '解锁MetaMask',
+    install_metaMask_tip: '安装MetaMask',
+    unlock_steps_title: '你要做的步骤',
+    unlock_refresh_button: '全做完了？刷新Circular',
+    unlock_step_install_title: '安装MetaMask',
+    unlock_step_install_content: '为你的浏览器安装MetaMask插件',
+    unlock_step_unlock_title: '解锁MetaMask',
+    unlock_step_unlock_content: '在MetaMask上创建一个账号或解锁已有账号',
+    unlock_step_refresh_title: '刷新Circular',
+    unlock_step_refresh_content: '刷新Circular钱包使MetaMask生效',
+  },
+  address:{
+    paste_address_title: '请粘贴您的地址',
+    invalid_address_tip: '不合法的地址',
+  },
+  wallet_determine:{
+    default_address: '默认地址',
+    title_deter_address: "选择地址",
+    actions_other_address: '选择其他地址',
+    title_select_path: '选择dpath',
+    custom_path: '自定义dpath',
+    no_address_tip: '没有合法的地址',
+  },
+  json:{
+    error_json_tip: '无效的Keystore Json',
+    title_json: '选择JSON文件',
+  },
+
+  mnemonic:{
+    actions_paste_mnemonic: '请粘贴您的助记词',
+    error_mnemonic_tip: "无效的助记词",
+    mnemonic_tip_lack: "请输入您的助记词"
+  },
+
+  key: {
+    paste_private_title: "请粘贴您的私钥",
+    error_private_tip: '不合法的私钥',
+  },
+
+
+
+
+
+
   token: {
     action_options: '{token} 选项',
     action_types: {
@@ -316,9 +337,6 @@ export default {
     actions_confirm_send: "确认转账",
     actions_confirm_convert: '确认转换'
   },
-  transaction: {
-
-
-  }
+  transaction: {}
 }
 
