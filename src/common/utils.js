af4d01965e7b5ea653dafe15f2283fdbf6bff4d9
lyd <lyddream@126.com>
2018-06-06 16:32:14 +0800

    Squashed 'src/common/loopringjs/' changes from 106cc30..a8e1e01
    a8e1e01 Merge pull request #91 from Loopring/v2
    1f7217f fix version
    5191c7d Merge branch 'master' into v2
    3f36a3a fix relay
    e9aaa54 Merge pull request #92 from Loopring/du
    28d17c7 fix readme
    fcf59f2 Merge remote-tracking branch 'origin/master' into du
    70cdf92 relau
    edecfa6 webpack
    192fcca Merge pull request #90 from Loopring/du
    d011183 gas
    d386faf gas
    370bcda Merge pull request #89 from Loopring/v2
    d3ce670 fix hash
    3d977ce Merge pull request #87 from Loopring/v2
    79ecdf1 merge
    83ad084 Merge branch 'master' into v2
    c926bdd remove unused module
    44c561b webpack
    a097205 Merge pull request #88 from Loopring/du
    a19124f hash
    2e03d99 Merge branch 'master' into v2
    7a94c4a fix account
    763e912 prepare for publish
    
    git-subtree-dir: src/common/loopringjs
    git-subtree-split: a8e1e01a02fa83ce4b7fd7b81e95c376b6203ba0

--- src/common/utils.js
+++ src/common/utils.js
@@ -1,15 +1,30 @@
+import {keccak} from 'ethereumjs-util';
+import {toHex, toBig} from './formatter';
+
 /**
  * trim head space and tail space
  * @param str string
  */
-export function trim(str) {
-  return str.replace(/(^\s+)|(\s+$)/g, "");
+export function trim (str)
+{
+    return str.replace(/(^\s+)|(\s+$)/g, '');
 }
 
 /**
  * trim all spaces
  * @param str
  */
-export function trimAll(str) {
-  return trim(str).replace(/\s/g, "");
+export function trimAll (str)
+{
+    return trim(str).replace(/\s/g, '');
+}
+
+export function keccakHash (str)
+{
+    return toHex(keccak(str));
+}
+
+export function calculateGas (gasPrice, gasLimit)
+{
+    return toBig(gasPrice).times(gasLimit).div(1e9);
 }
