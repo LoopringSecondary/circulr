88a6b8b1e00614a75f62b35582df25e652214eb0
dyfvicture <fbi-ceo@163.com>
2018-06-07 13:30:28 +0800

    loopr

--- src/components/orders/PlaceOrderByLoopr.js
+++ src/components/orders/PlaceOrderByLoopr.js
@@ -4,9 +4,11 @@ import intl from 'react-intl-universal'
 import Alert from 'LoopringUI/components/Alert'
 import PlaceOrderResult from './PlaceOrderResult'
 import {connect} from 'dva'
-const PlaceOrderByLoopr = ({
-    form
-  }) => {
+import QRCode from 'qrcode.react';
+
+const PlaceOrderByLoopr = (props) => {
+  const {form, wallet, placeOrder, placeOrderByLoopr} = props
+
   function handleSubmit() {
     form.validateFields((err,values) => {
       console.log('values',values);
@@ -36,21 +38,22 @@ const PlaceOrderByLoopr = ({
     <Card className="rs" title={<div className="pl10 ">Place Order By Loopr Wallet</div>}>
       <div className="p15">
         <div className="mb20 mt15">
-          <Steps current={0}>
+          <Steps current={placeOrderByLoopr.step}>
            {steps.map(item => <Steps.Step key={item.title} title={item.title} />)}
           </Steps>
         </div>
         {
-          true &&
+          placeOrderByLoopr.step === 0 &&
           <div className="mt15">
             <div className="zb-b">
               <div className="text-center p15">
-                <img src="http://pic.36krcnd.com/avatar/201612/07071825/57rrbw8czufu0hqm.png" alt="" style={{width:'320px',height:'320px'}}/>
+                {placeOrderByLoopr.qrcode && <QRCode value={placeOrderByLoopr.qrcode} size={320} level='H'/>}
                 <div className="pt10 pb10 color-black-2 text-left fs12 " style={{width:'320px',margin:'0 auto'}}>
                   1. 下载 Loopr-IOS
                   <br />
                   2. 打开 Loopr Wallet，点击扫码
                   <br />
+                  * 二维码有效时间24小时，请尽快完成扫码操作，过期后请重新下单生成二维码
                 </div>
               </div>
             </div>
@@ -58,7 +61,7 @@ const PlaceOrderByLoopr = ({
           </div>
         }
         {
-          true &&
+          placeOrderByLoopr.step === 1 &&
           <div className="mt15">
             <div className="zb-b">
                 <div className="text-center p35">
@@ -70,19 +73,23 @@ const PlaceOrderByLoopr = ({
           </div>
         }
         {
-          true &&
+          placeOrderByLoopr.step === 2 &&
           <div className="mt15">
             <PlaceOrderResult />
           </div>
         }
-
-
       </div>
     </Card>
   );
 };
 
-
-export default Form.create()(connect()(PlaceOrderByLoopr));
+function mapToProps(state) {
+  return {
+    wallet:state.wallet,
+    placeOrder:state.placeOrder,
+    placeOrderByLoopr:state.placeOrderByLoopr
+  }
+}
+export default Form.create()(connect(mapToProps)(PlaceOrderByLoopr));
 
 
