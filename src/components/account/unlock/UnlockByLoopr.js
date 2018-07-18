9b5193ec8b7a331a7669bf9aa7e0b21fab99dbe3
lyd <lyddream@126.com>
2018-07-18 11:02:53 +0800

    fix socket

--- src/components/account/unlock/UnlockByLoopr.js
+++ src/components/account/unlock/UnlockByLoopr.js
@@ -1,8 +1,5 @@
 import React from 'react';
-import {Button, Modal, Steps} from 'antd';
 import {connect} from 'dva'
-import routeActions from 'common/utils/routeActions'
-import Notification from '../../../common/loopringui/components/Notification'
 import intl from 'react-intl-universal';
 import QRCode from 'qrcode.react';
 import CountDown from 'LoopringUI/components/CountDown';
@@ -14,36 +11,15 @@ function UnlockByLoopr(props) {
   let targetTime = moment().valueOf() + 600000;
 
   const refresh = () => {
-    const uuid = uuidv4()
-    props.dispatch({type:'scanAddress/uuidChanged', payload:{UUID:uuid.substring(0, 8)}})
-    props.dispatch({type:'sockets/extraChange',payload:{id:'addressUnlock', extra:{UUID:uuid.substring(0, 8)}}});
-    props.dispatch({type:'sockets/fetch',payload:{id:'addressUnlock'}});
+    const uuid = uuidv4();
+    dispatch({type:'scanAddress/uuidChanged', payload:{UUID:uuid.substring(0, 8)}});
+    dispatch({type:'sockets/extraChange',payload:{id:'addressUnlock', extra:{UUID:uuid.substring(0, 8)}}});
+    dispatch({type:'sockets/fetch',payload:{id:'addressUnlock'}});
     targetTime = moment().valueOf() + 600000;
-  }
-
-  const unlock = () => {
-    const address = scanAddress.address
-    if(!address) {
-      return
-    }
-    dispatch({type:'scanAddress/addressChanged', payload:{address}})
-    dispatch({type:"wallet/unlockAddressWallet",payload:{address}});
-    Notification.open({type:'success',message:intl.get('notifications.title.unlock_suc')});
-    dispatch({type: 'sockets/unlocked'});
-    routeActions.gotoPath('/wallet');
-    dispatch({type:'layers/hideLayer', payload:{id:'unlock'}})
-    dispatch({type:'scanAddress/reset', payload:{}})
   }
 
   return (
     <div className="d-flex justify-content-center">
-      {
-        false && scanAddress.address &&
-        <div>
-          <div>Address: {scanAddress.address}</div>
-          <div><Button className="mt15" type="default" onClick={unlock}> Unlock In Watch Only Mode </Button></div>
-        </div>
-      }
       {
         !scanAddress.address &&
         <div className="">
